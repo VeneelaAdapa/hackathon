976f320d0d9e5d8bc55f90f8dc874999ef617d27
VeneelaAdapa <48537835+VeneelaAdapa@users.noreply.github.com>
2020-03-23 17:57:05 +0530

    Update server.js

--- Login_v4/server.js
+++ Login_v4/server.js
@@ -75,6 +75,9 @@ app.get('/', function(request, response) {
 app.get("/register-html", async (request, response) => {
     return response.sendFile(__dirname + '/register.html');
 });
+app.get("/login-html", async (request, response) => {
+    return response.sendFile(__dirname + '/login.html');
+});
 app.post("/register", async (request, response) => {
     
     try {
